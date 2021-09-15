54acdcb786547408bed140900374326017b216b8
Nelson Silva <nelsonsilvadev@gmail.com>
2021-09-15 16:13:12 +0100

    Add 03-Operadores-Aritmeticos (Python)

---
+++ Programar em Python/03-Operadores-Aritmeticos.py
@@ -0,0 +1,6 @@
+print('Adição:', 10 + 10) # 20
+print('Subtração:', 10.5 - 5.5 - 4.5) # 0.5
+print('Multiplicação:', -10 * -2) # -20
+print('Divisão:', 10 / 10) # 1.0
+print('Resto:', 8 % 2) # 0
+print('Exponente:', 2 ** 5) # 2 * 2 * 2 * 2 * 2 = 32
