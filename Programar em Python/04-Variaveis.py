a87a97739a36da8e46c2b605d201adf2f1353617
Nelson Silva <nelsonsilvadev@gmail.com>
2021-09-16 11:36:15 +0100

    Add 04-Variaveis (Python)

---
+++ Programar em Python/04-Variaveis.py
@@ -0,0 +1,9 @@
+inteiro = 10
+decimal = 10.5
+string = 'Eu sou uma string.'
+booleano = True
+
+print('inteiro:', inteiro) # 10
+print('decimal:', decimal) # 10.5
+print('string:', string) # Eu sou uma string.
+print('booleano:', booleano) # True
