20871b8964896250099b0fd6af08fbb56226d55f
Nelson Silva <nelsonsilvadev@gmail.com>
2021-09-20 23:41:36 +0100

    Add 06-Mais-sobre-as-Strings (Python)

---
+++ Programar em Python/06-Mais-sobre-as-Strings.py
@@ -0,0 +1,16 @@
+string = 'Eu sou\n\tuma string.'
+
+print(len(string)) # 19
+print(string.upper())
+
+'''
+  EU SOU
+    UMA STRING.
+'''
+
+print(string.lower())
+
+'''
+  eu sou
+    uma string.
+'''
