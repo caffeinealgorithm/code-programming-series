46184956cd4d81b877cd647be678bc90820ef36d
Nelson Silva <nelsonsilvadev@gmail.com>
2021-09-22 18:22:16 +0100

    Add 08-Funcao-input (Python)

---
+++ Programar em Python/08-Funcao-input.py
@@ -0,0 +1,7 @@
+primeiroNome = input('Insere o teu primeiro nome: ')
+ultimoNome = input('Insere o teu último nome: ')
+idade = input('Insere a tua idade: ')
+
+print('Primeiro nome:', primeiroNome)
+print('Último nome:', ultimoNome)
+print('Idade:', idade)
