4e684cd67e688b97afd9470cb01e1d60416df233
Nelson Silva <nelsonsilvadev@gmail.com>
2021-09-23 18:20:43 +0100

    Add 09-Listas (Python)

---
+++ Programar em Python/09-Listas.py
@@ -0,0 +1,7 @@
+lista = ['Eu sou uma string.', 10, 10.5, True, ['A', 'B', 'C']]
+
+lista[0] = 'Eu continuo a ser uma string na mesma.'
+
+print('Fatiamento:', lista[2:3]) # Fatiamento: [10.5]
+print('Conteúdo da lista:', lista)
+print('Número de elementos da lista:', len(lista[4])) # Número de elementos da lista: 3
