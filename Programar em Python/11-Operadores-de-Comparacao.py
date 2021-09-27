83dfdd01126022eb4cc3c3b2457588b07db2e5e8
Nelson Silva <nelsonsilvadev@gmail.com>
2021-09-27 18:22:19 +0100

    Add 11-Operadores-de-Comparacao (Python)

---
+++ Programar em Python/11-Operadores-de-Comparacao.py
@@ -0,0 +1,18 @@
+x = 10
+y = 20
+
+'''
+  if x == y
+  if x != y
+  if x > y
+  if x < y
+  if x >= y
+  if x <= y
+  if x <= y and x == y:
+'''
+if x <= y or x == y:
+  print('Esta condição é verdadeira.')
+else:
+  print('Esta condição é falsa.')
+
+# Esta condição é verdadeira.
