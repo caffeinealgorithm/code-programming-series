5c8f3b3170b7ca687bacbe6fd95b6c14101aae70
Nelson Silva <nelsonsilvadev@gmail.com>
2021-09-28 17:46:20 +0100

    Add 12-Ciclo-for (Python)

---
+++ Programar em Python/12-Ciclo-for.py
@@ -0,0 +1,26 @@
+organizacao = 'Caffeine Algorithm'
+cores = ['Azul', 'Verde', 'Amarelo', 'Vermelho', 'Laranja']
+
+'''
+  for carater in organizacao:
+    print('Carater:', carater)
+
+  for cor in cores:
+    print('Cor:', cor)
+'''
+
+for numero in range(1, 11):
+  print('Número:', numero)
+
+'''
+  Número: 1
+  Número: 2
+  Número: 3
+  Número: 4
+  Número: 5
+  Número: 6
+  Número: 7
+  Número: 8
+  Número: 9
+  Número: 10
+'''
