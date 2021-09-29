2552d898761846f780e850fbbaffcc158e6ef281
Nelson Silva <nelsonsilvadev@gmail.com>
2021-09-29 18:05:37 +0100

    Add 13-Ciclo-while (Python)

---
+++ Programar em Python/13-Ciclo-while.py
@@ -0,0 +1,23 @@
+'''
+  while True:
+    print('Estamos perante um ciclo infinito.')
+'''
+
+contador = 1
+
+while contador <= 10:
+  print('Número da volta:', contador)
+  contador += 1
+
+'''
+  Número da volta: 1
+  Número da volta: 2
+  Número da volta: 3
+  Número da volta: 4
+  Número da volta: 5
+  Número da volta: 6
+  Número da volta: 7
+  Número da volta: 8
+  Número da volta: 9
+  Número da volta: 10
+'''
