d1395848c7674405d37a298d4740ccc5c6846b8b
Nelson Silva <nelsonsilvadev@gmail.com>
2021-10-01 17:32:24 +0100

    Add 15-Funcoes (Python)

---
+++ Programar em Python/15-Funcoes.py
@@ -0,0 +1,20 @@
+def dadosPessoais():
+  print('Nome: Nelson Silva')
+  print('Idade: 25')
+  print('Nacionalidade: Portuguesa')
+
+dadosPessoais()
+dadosPessoais()
+dadosPessoais()
+
+'''
+  Nome: Nelson Silva
+  Idade: 25
+  Nacionalidade: Portuguesa
+  Nome: Nelson Silva
+  Idade: 25
+  Nacionalidade: Portuguesa
+  Nome: Nelson Silva
+  Idade: 25
+  Nacionalidade: Portuguesa
+'''
