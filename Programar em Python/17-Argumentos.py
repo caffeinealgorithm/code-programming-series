7e83ee39c097e7d8e7d51d3c0e04496a7ecdc32f
Nelson Silva <nelsonsilvadev@gmail.com>
2021-10-06 17:58:18 +0100

    Add 17-Argumentos (Python)

---
+++ Programar em Python/17-Argumentos.py
@@ -0,0 +1,16 @@
+def dadosPessoais(nome, idade, nacionalidade):
+  print('Nome:', nome)
+  print('Idade:', idade)
+  print('Nacionalidade:', nacionalidade)
+
+dadosPessoais('Nelson Silva', 25, 'Portuguesa')
+dadosPessoais('Larissa Fernandes', 37, 'Brasileira')
+
+'''
+  Nome: Nelson Silva
+  Idade: 25
+  Nacionalidade: Portuguesa
+  Nome: Larissa Fernandes
+  Idade: 37
+  Nacionalidade: Brasileira
+'''
