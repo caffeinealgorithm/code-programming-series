cf1372ff497d3c299572e46a1fd17ac1c172a473
Nelson Silva <nelsonsilvadev@gmail.com>
2021-10-08 17:48:20 +0100

    Add 19-Argumentos-Chave (Python)

---
+++ Programar em Python/19-Argumentos-Chave.py
@@ -0,0 +1,12 @@
+def imprimirABC(a, b, c):
+  print('Valor de a:', a)
+  print('Valor de b:', b)
+  print('Valor de c:', c)
+
+imprimirABC(c = 1, a = 2, b = 3)
+
+'''
+  Valor de a: 2
+  Valor de b: 3
+  Valor de c: 1
+'''
