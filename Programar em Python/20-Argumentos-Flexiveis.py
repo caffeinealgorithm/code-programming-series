dfa26692c26fd8311b1eb55747dfa8c78f9475df
Nelson Silva <nelsonsilvadev@gmail.com>
2021-10-11 17:48:18 +0100

    Add 20-Argumentos-Flexiveis (Python)

---
+++ Programar em Python/20-Argumentos-Flexiveis.py
@@ -0,0 +1,13 @@
+def imprimirArgumentos(*argumentos):
+  for argumento in argumentos:
+    print('Argumento:', argumento)
+
+imprimirArgumentos('Eu sou uma string.', 1, 1.5, True, ['ABC', 'DEF', 'GHI'])
+
+'''
+  Argumento: Eu sou uma string.
+  Argumento: 1
+  Argumento: 1.5
+  Argumento: True
+  Argumento: ['ABC', 'DEF', 'GHI']
+'''
