ca024164d0d5104a62551c9821eec686ecc3ddea
Nelson Silva <nelsonsilvadev@gmail.com>
2021-10-13 17:56:24 +0100

    Add 22-Erros-Comuns (Python)

---
+++ Programar em Python/22-Erros-Comuns.py
@@ -0,0 +1,9 @@
+variavel = 'Eu sou uma string.'
+print(variavell) # NameError: name 'variavell' is not defined
+
+def funcao():
+print('Eu faço parte desta função.') # IndentationError: expected an indented block
+
+print('Eu também sou uma string.)' # SyntaxError: EOL while scanning string literal
+
+print('Somos todos uma string.' # SyntaxError: invalid syntax
