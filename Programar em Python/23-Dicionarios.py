13dad97e8f23b25e179ef66b743600a1d238dca5
Nelson Silva <nelsonsilvadev@gmail.com>
2021-10-14 14:56:36 +0100

    Add 23-Dicionarios (Python)

---
+++ Programar em Python/23-Dicionarios.py
@@ -0,0 +1,10 @@
+idades = {'Nelson Silva': 25, 'Larissa Fernandes': 37, 'Pedro Henrique': 52, 'Raquel Soares': 68}
+
+idades['Pedro Henrique'] = 100
+del idades['Larissa Fernandes']
+# idades.clear()
+
+print(idades) # {'Nelson Silva': 25, 'Pedro Henrique': 100, 'Raquel Soares': 68}
+print(idades.items()) # dict_items([('Nelson Silva', 25), ('Pedro Henrique', 100), ('Raquel Soares', 68)])
+print(idades.keys()) # dict_keys(['Nelson Silva', 'Pedro Henrique', 'Raquel Soares'])
+print(idades.values()) # dict_values([25, 100, 68])
