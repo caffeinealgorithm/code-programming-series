d23b348dfe7c97065b72d73d6aba1e6c9310e90c
Nelson Silva <nelsonsilvadev@gmail.com>
2021-10-18 17:47:51 +0100

    Add 25-Funcoes-Matematicas (Python)

---
+++ Programar em Python/25-Funcoes-Matematicas.py
@@ -0,0 +1,12 @@
+from math import *
+
+numeros = [10, 20, 30, 40, 50]
+
+print('Resultado da função sin():', sin(10.5)) # Resultado da função sin(): -0.87969575997167
+print('Resultado da função cos():', cos(10.5)) # Resultado da função cos(): -0.4755369279959925
+print('Resultado da função tan():', tan(10.5)) # Resultado da função tan(): 1.8498999934219273
+print('Resultado da função floor():', floor(10.5)) # Resultado da função floor(): 10
+print('Resultado da função ceil():', ceil(10.5)) # Resultado da função ceil(): 11
+print('Resultado da função abs():', abs(-10.5)) # Resultado da função abs(): 10.5
+print('Resultado da função fsum():', fsum(numeros)) # Resultado da função fsum(): 150.0
+print('Resultado da função pow():', pow(2, 5)) # Resultado da função pow(): 32.0
