2a7e92aa45a8fa9203c08f292da4a4733b92cd1c
Nelson Silva <nelsonsilvadev@gmail.com>
2021-10-26 18:33:00 +0100

    Add 31-Heranca (Python)

---
+++ Programar em Python/31-Heranca.py
@@ -0,0 +1,26 @@
+# YouTube: https://youtu.be/7cVfMDDAk64
+# Publicação: https://caffeinealgorithm.com/blog/20211026/heranca-em-python/
+
+class Pai:
+  ultimoNome = 'Silva'
+
+  def comidaPreferida(self):
+    print('A minha comida preferida é arroz de marisco.')
+
+class Filho(Pai):
+  primeiroNome = 'Nelson'
+  idade = 25
+
+  def informacao(self):
+    print('Nome:', self.primeiroNome, self.ultimoNome)
+    print('Idade:', self.idade)
+
+pessoa = Filho()
+pessoa.informacao()
+pessoa.comidaPreferida()
+
+'''
+  Nome: Nelson Silva
+  Idade: 25
+  A minha comida preferida é arroz de marisco.
+'''
