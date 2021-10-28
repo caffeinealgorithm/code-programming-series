cb702a42a3a0a6b6165967294b9b838335c7f3b5
Nelson Silva <nelsonsilvadev@gmail.com>
2021-10-28 18:14:04 +0100

    Add 33-Lambda (Python)

---
+++ Programar em Python/33-Lambda.py
@@ -0,0 +1,11 @@
+# YouTube: https://youtu.be/0bSYL5vnh6k
+# Publicação: https://caffeinealgorithm.com/blog/20211028/lambda-em-python/
+
+'''
+  def adicao(a, b, c):
+    return a + b + c
+'''
+
+adicao = (lambda a, b, c : a + b + c)
+
+print('Resultado da adição:', adicao(25, 30, 45)) # Resultado da adição: 100
