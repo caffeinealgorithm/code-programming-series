4c70437dbd47914da0667af75206df2656b4643b
Nelson Silva <nelsonsilvadev@gmail.com>
2021-11-02 18:11:52 +0000

    Add 35-Operadores-Bit-a-Bit (Python)

---
+++ Programar em Python/35-Operadores-Bit-a-Bit.py
@@ -0,0 +1,10 @@
+# YouTube: https://youtu.be/gyBY7AcVPk4
+# Publicação: https://caffeinealgorithm.com/blog/20211102/operadores-bit-a-bit-em-python/
+
+x = 60 # 00111100
+y = 13 # 00001101
+
+print('Resultado do operador &:', x & y) # Resultado do operador &: 12 (00001100)
+print('Resultado do operador |:', x | y) # Resultado do operador |: 61 (00111101)
+print('Resultado do operador <<:', x << 2) # Resultado do operador <<: 240 (11110000)
+print('Resultado do operador >>:', x >> 2) # Resultado do operador >>: 15 (00001111)
