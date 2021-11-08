258b50b74da6da46352e2430ce580ad31ced5583
Nelson Silva <nelsonsilvadev@gmail.com>
2021-11-08 18:04:03 +0000

    Add 04-OperadoresAritmeticos (C#)

---
+++ Programar em C#/04-OperadoresAritmeticos.cs
@@ -0,0 +1,16 @@
+﻿// YouTube: https://youtu.be/x-xdi7NRJDs
+// Publicação: https://caffeinealgorithm.com/blog/20211108/operadores-aritmeticos-em-csharp/
+
+using System;
+
+namespace Base {
+  class OperadoresAritmeticos {
+    public void Run() {
+      Console.WriteLine("Adição: " + (10 + 10)); // Adição: 20
+      Console.WriteLine("Subtração: " + 10.5 - 5.5 - 4.5); // Subtração: 0,5
+      Console.WriteLine("Multiplicação: " + -10 * 2); // Multiplicação: -20
+      Console.WriteLine("Divisão: " + 10 / 10); // Divisão: 1
+      Console.WriteLine("Resto: " + 8 % 2); // Resto: 0
+    }
+  }
+}
