3913b30c7516b6e6575f8691351415a0ff4ca38d
Nelson Silva <nelsonsilvadev@gmail.com>
2021-11-09 17:01:05 +0000

    Add 05-Variaveis (C#)

---
+++ Programar em C#/05-Variaveis.cs
@@ -0,0 +1,22 @@
+﻿// YouTube: https://youtu.be/H1D_3_XgI9E
+// Publicação: https://caffeinealgorithm.com/blog/20211109/variaveis-em-csharp/
+
+using System;
+
+namespace Base {
+  class Variaveis {
+    public void Run() {
+      int inteiro = 10;
+      double _double = 10.5;
+      string _string = "Eu sou uma string.";
+      char carater = 'c';
+      bool booleano = true;
+
+      Console.WriteLine("inteiro: " + inteiro); // inteiro: 10
+      Console.WriteLine("double: " + _double); // double: 10,5
+      Console.WriteLine("string: " + _string); // string: Eu sou uma string.
+      Console.WriteLine("carater: " + carater); // carater: c
+      Console.WriteLine("booleano: " + booleano); // booleano: True
+    }
+  }
+}
