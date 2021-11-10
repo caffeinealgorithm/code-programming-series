f4591091d6b4ff3c57a02f580107a51bb2ab19d5
Nelson Silva <nelsonsilvadev@gmail.com>
2021-11-10 18:30:56 +0000

    Add 06-OperadoresDeAtribuicao (C#)

---
+++ Programar em C#/06-OperadoresDeAtribuicao.cs
@@ -0,0 +1,27 @@
+﻿// YouTube: https://youtu.be/mKXawyHLewc
+// Publicação: https://caffeinealgorithm.com/blog/20211110/operadores-de-atribuicao-em-csharp/
+
+using System;
+
+namespace Base {
+  class OperadoresDeAtribuicao {
+    public void Run() {
+      int x = 2;
+
+      x += 3;
+      Console.WriteLine("x = x + 3: " + x); // x = x + 3: 5
+
+      x -= 2;
+      Console.WriteLine("x = x - 2: " + x); // x = x - 2: 3
+
+      x *= 2;
+      Console.WriteLine("x = x * 2: " + x); // x = x * 2: 6
+
+      x /= 2;
+      Console.WriteLine("x = x / 2: " + x); // x = x / 2: 3
+
+      x %= 3;
+      Console.WriteLine("x = x % 3: " + x); // x = x % 3: 0
+    }
+  }
+}
