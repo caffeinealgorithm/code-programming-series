7f350174d74ee3dad06b2b33b7e1ef8be26ddb94
Nelson Silva <nelsonsilvadev@gmail.com>
2021-11-11 18:16:35 +0000

    Add 07-MaisSobreAsStrings (C#)

---
+++ Programar em C#/07-MaisSobreAsStrings.cs
@@ -0,0 +1,21 @@
+﻿// YouTube: https://youtu.be/pKGz-CCpye8
+// Publicação: https://caffeinealgorithm.com/blog/20211111/mais-sobre-as-strings-em-csharp/
+
+using System;
+
+namespace Base {
+  class MaisSobreAsStrings {
+    public void Run() {
+      string primeiroNome = "Nelson";
+      string ultimoNome = "Silva";
+      int idade = 25;
+
+      Console.WriteLine($"Nome: {primeiroNome} {ultimoNome}\nIdade: {idade}");
+
+      /*
+        Nome: Nelson Silva
+        Idade: 25
+      */
+    }
+  }
+}
