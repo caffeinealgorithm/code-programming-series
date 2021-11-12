70f72aad96e51cbc323873f3bcff42b4e646a7f0
Nelson Silva <nelsonsilvadev@gmail.com>
2021-11-12 18:29:27 +0000

    Add 08-Comentarios (C#)

---
+++ Programar em C#/08-Comentarios.cs
@@ -0,0 +1,22 @@
+﻿// YouTube: https://youtu.be/CFrR5_JH6Nw
+// Publicação: https://caffeinealgorithm.com/blog/20211112/comentarios-em-csharp/
+
+using System;
+
+namespace Base {
+  class Comentarios {
+    public void Run() {
+      // Eu sou um comentário.
+
+      Console.WriteLine("Eu sou uma string.");
+
+      /*
+        Eu também sou um comentário.
+        Eu faço parte deste mesmo comentário.
+        Continuo a fazer parte dele.
+      */
+
+      Console.WriteLine("Eu continuo a ser uma string.");
+    }
+  }
+}
