1c6f8ccce89479999d4d040641560128c052b482
Nelson Silva <nelsonsilvadev@gmail.com>
2021-11-15 14:51:41 +0000

    Add 09-FuncaoReadLine (C#)

---
+++ Programar em C#/09-FuncaoReadLine.cs
@@ -0,0 +1,24 @@
+﻿// YouTube: https://youtu.be/-C6_spcZYAs
+// Publicação: https://caffeinealgorithm.com/blog/20211115/funcao-readline-em-csharp/
+
+using System;
+
+namespace Base {
+  class FuncaoReadLine {
+    public void Run() {
+      string primeiroNome, ultimoNome;
+      int idade;
+
+      Console.Write("Insere o teu primeiro nome: ");
+      primeiroNome = Console.ReadLine();
+
+      Console.Write("Insere o teu último nome: ");
+      ultimoNome = Console.ReadLine();
+
+      Console.Write("Insere a tua idade: ");
+      idade = int.Parse(Console.ReadLine());
+
+      Console.WriteLine($"Nome: {primeiroNome} {ultimoNome}\nIdade: {idade}");
+    }
+  }
+}
