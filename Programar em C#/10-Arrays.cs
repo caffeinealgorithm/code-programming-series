af73f0ed310886877bbfe7c13c85921cde3232b7
Nelson Silva <nelsonsilvadev@gmail.com>
2021-11-16 17:28:51 +0000

    Add 10-Arrays (C#)

---
+++ Programar em C#/10-Arrays.cs
@@ -0,0 +1,32 @@
+﻿// YouTube: https://youtu.be/Us3VJyBFbbY
+// Publicação: https://caffeinealgorithm.com/blog/20211116/arrays-em-csharp/
+
+using System;
+
+namespace Base {
+  class Arrays {
+    public void Run() {
+      /*
+        string[] cores = new string[5];
+
+        cores[0] = "Azul";
+        cores[1] = "Verde";
+        cores[2] = "Amarelo";
+        cores[3] = "Vermelho";
+        cores[4] = "Laranja";
+      */
+
+      string[] cores = new string[] {
+        "Azul",
+        "Verde",
+        "Amarelo",
+        "Vermelho",
+        "Laranja"
+      };
+
+      Console.WriteLine($"Número de cores: {cores.Length}"); // Número de cores: 5
+      Console.WriteLine($"Primeira cor: {cores[0]}"); // Primeira cor: Azul
+      Console.WriteLine($"Última cor: {cores[cores.Length - 1]}"); // Última cor: Laranja
+    }
+  }
+}
