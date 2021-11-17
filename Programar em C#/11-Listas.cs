28a780dfecc47344cf9a40f0ff35953a83dba518
Nelson Silva <nelsonsilvadev@gmail.com>
2021-11-17 14:04:06 +0000

    Add 11-Listas (C#)

---
+++ Programar em C#/11-Listas.cs
@@ -0,0 +1,29 @@
+﻿// YouTube: https://youtu.be/Eov-gcwg9zc
+// Publicação: https://caffeinealgorithm.com/blog/20211117/listas-em-csharp/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Base {
+  class Listas {
+    public void Run() {
+      List<string> cores = new List<string>();
+
+      cores.Add("Azul");
+      cores.Add("Verde");
+      cores.Add("Amarelo");
+      cores.Add("Vermelho");
+      cores.Add("Laranja");
+
+      cores.Remove("Laranja");
+      cores.Reverse();
+      cores.Sort();
+      // cores.Clear();
+
+      Console.WriteLine($"Número de cores: {cores.Count}"); // Número de cores: 4
+      Console.WriteLine($"Primeira cor: {cores.First()}"); // Primeira cor: Amarelo
+      Console.WriteLine($"Última cor: {cores.Last()}"); // Última cor: Vermelho
+    }
+  }
+}
