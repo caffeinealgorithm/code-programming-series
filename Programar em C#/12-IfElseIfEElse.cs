730720af46e973cdda29ef45af673cb538745a48
Nelson Silva <nelsonsilvadev@gmail.com>
2021-11-18 16:39:54 +0000

    Add 12-IfElseIfEElse (C#)

---
+++ Programar em C#/12-IfElseIfEElse.cs
@@ -0,0 +1,36 @@
+﻿// YouTube: https://youtu.be/x7FJnvDzVtg
+// Publicação: https://caffeinealgorithm.com/blog/20211118/if-else-if-e-else-em-csharp/
+
+using System;
+
+namespace Base {
+  class IfElseIfEElse {
+    /*
+      (if - se) condição for verdadeira {
+        o código dentro do if é executado
+      }
+      (else if - senão se) condição for verdadeira (só ocorre o else if caso a condição de if seja falsa) {
+        o código dentro do else if é executado
+      }
+      (else - senão) sem condição (só ocorre caso a condição de if e de else if sejam falsas) {
+        o código dentro do else é executado
+      }
+    */
+
+    public void Run() {
+      int x = 30;
+
+      if (x == 10) {
+        Console.WriteLine("O valor de x é igual a 10.");
+      }
+      else if (x == 20) {
+        Console.WriteLine("O valor de x é igual a 20.");
+      }
+      else {
+        Console.WriteLine("O valor de x é diferente de 10 e de 20.");
+      }
+    }
+  }
+}
+
+// O valor de x é diferente de 10 e de 20.
