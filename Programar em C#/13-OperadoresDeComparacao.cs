f36b4ac8037be2154296720ee939bfd47512cb08
Nelson Silva <nelsonsilvadev@gmail.com>
2021-11-19 15:40:07 +0000

    Add 13-OperadoresDeComparacao (C#)

---
+++ Programar em C#/13-OperadoresDeComparacao.cs
@@ -0,0 +1,22 @@
+﻿// YouTube: https://youtu.be/Vg8cCo5bIic
+// Publicação: https://caffeinealgorithm.com/blog/20211119/operadores-de-comparacao-em-csharp/
+
+using System;
+
+namespace Base {
+  class OperadoresDeComparacao {
+    public void Run() {
+      int x = 10;
+      int y = 20;
+
+      if (x <= y || x == y) {
+        Console.WriteLine("Esta condição é verdadeira.");
+      }
+      else {
+        Console.WriteLine("Esta condição é falsa.");
+      }
+    }
+  }
+}
+
+// Esta condição é verdadeira.
