33f391b12da0820aa2ca65277c46e06c3b37bcb0
Nelson Silva <nelsonsilvadev@gmail.com>
2021-11-22 18:39:57 +0000

    Add 14-OperadorTernario (C#)

---
+++ Programar em C#/14-OperadorTernario.cs
@@ -0,0 +1,30 @@
+﻿// YouTube: https://youtu.be/KMHQmYq6Kms
+// Publicação: https://caffeinealgorithm.com/blog/20211122/operador-ternario-em-csharp/
+
+using System;
+
+namespace Base {
+  class OperadorTernario {
+    // (condição) ? condição for verdadeira : condição for falsa
+
+    public void Run() {
+      int idade = 25;
+      bool eMaiorDeIdade;
+
+      /*
+        if (idade >= 18) {
+          eMaiorDeIdade = true;
+        }
+        else {
+          eMaiorDeIdade = false;
+        }
+      */
+
+      eMaiorDeIdade = (idade >= 18) ? true : false;
+
+      Console.WriteLine($"É maior de idade: {eMaiorDeIdade}");
+    }
+  }
+}
+
+// É maior de idade: True
