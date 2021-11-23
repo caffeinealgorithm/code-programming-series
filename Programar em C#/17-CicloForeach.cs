91e24ee62d45ef8495dc7b0801b69ea3db7571d3
Nelson Silva <nelsonsilvadev@gmail.com>
2021-11-23 19:52:29 +0000

    Add 17-CicloForeach (C#)

---
+++ Programar em C#/17-CicloForeach.cs
@@ -0,0 +1,42 @@
+﻿// YouTube: https://youtu.be/seMBMqxe8KA
+// Publicação: https://caffeinealgorithm.com/blog/20211125/ciclo-foreach-em-csharp/
+
+using System;
+using System.Collections.Generic;
+
+namespace Base {
+  class CicloForeach {
+    public void Run() {
+      string organizacao = "Caffeine Algorithm";
+      List<string> paises = new List<string>() {
+        "Portugal",
+        "Brasil",
+        "Espanha",
+        "França",
+        "Itália",
+        "Austrália",
+        "Índia"
+      };
+
+      /*
+        foreach (char carater in organizacao) {
+          Console.WriteLine($"Carater: {carater}");
+        }
+      */
+
+      foreach (string pais in paises) {
+        Console.WriteLine($"País: {pais}");
+      }
+    }
+  }
+}
+
+/*
+  País: Portugal
+  País: Brasil
+  País: Espanha
+  País: França
+  País: Itália
+  País: Austrália
+  País: Índia
+*/
