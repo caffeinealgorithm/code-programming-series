9d3792a13e00b9f4aadabce4b2bbe4cd76acd3e0
Nelson Silva <nelsonsilvadev@gmail.com>
2021-11-23 16:32:11 +0000

    Add 15-Switch (C#)

---
+++ Programar em C#/15-Switch.cs
@@ -0,0 +1,29 @@
+﻿// YouTube: https://youtu.be/vqC8km6r22k
+// Publicação: https://caffeinealgorithm.com/blog/20211123/switch-em-csharp/
+
+using System;
+
+namespace Base {
+  class Switch {
+    public void Run() {
+      char caso = 'D';
+
+      switch (caso) {
+        case 'A':
+          Console.WriteLine("O caso A existe.");
+          break;
+        case 'B':
+          Console.WriteLine("O caso B existe.");
+          break;
+        case 'C':
+          Console.WriteLine("O caso C existe.");
+          break;
+        default:
+          Console.WriteLine($"O caso {caso} não existe.");
+          break;
+      }
+    }
+  }
+}
+
+// O caso D não existe.
