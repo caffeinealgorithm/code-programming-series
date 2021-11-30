d045b6a5bd0884919c2085ea7ca01a460d6c187f
Nelson Silva <nelsonsilvadev@gmail.com>
2021-11-30 17:51:30 +0000

    Add 20-Funcoes (C#)

---
+++ Programar em C#/20-Funcoes.cs
@@ -0,0 +1,32 @@
+﻿// YouTube: https://youtu.be/61T_m11jCWs
+// Publicação: https://caffeinealgorithm.com/blog/20211130/funcoes-em-csharp/
+
+using System;
+
+namespace Base {
+  class Funcoes {
+    public void Run() {
+      DadosPessoais();
+      DadosPessoais();
+      DadosPessoais();
+    }
+
+    public void DadosPessoais() {
+      Console.WriteLine("Nome: Nelson Silva");
+      Console.WriteLine("Idade: 25");
+      Console.WriteLine("Nacionalidade: Portuguesa");
+    }
+  }
+}
+
+/*
+  Nome: Nelson Silva
+  Idade: 25
+  Nacionalidade: Portuguesa
+  Nome: Nelson Silva
+  Idade: 25
+  Nacionalidade: Portuguesa
+  Nome: Nelson Silva
+  Idade: 25
+  Nacionalidade: Portuguesa
+*/
