043ab483a125203c06f415abd41e7e95b1862d1f
Nelson Silva <nelsonsilvadev@gmail.com>
2021-12-02 13:51:36 +0000

    Add 21-Return (C#)

---
+++ Programar em C#/21-Return.cs
@@ -0,0 +1,23 @@
+﻿// YouTube: https://youtu.be/-WPMAO60yQ8
+// Publicação: https://caffeinealgorithm.com/blog/20211202/return-em-csharp/
+
+using System;
+
+namespace Base {
+  class Return {
+    public void Run() {
+      Console.WriteLine($"Resultado da adição: {Adicao()}");
+    }
+
+    public int Adicao() {
+      int resultado = 0;
+
+      for (int valor = 1; valor <= 10; valor++)
+        resultado += valor;
+
+      return resultado;
+    }
+  }
+}
+
+// Resultado da adição: 55
