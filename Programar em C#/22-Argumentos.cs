40a81c0207fb2f35966b221bc7d90335fe965af9
Nelson Silva <nelsonsilvadev@gmail.com>
2021-12-03 18:47:19 +0000

    Add 22-Argumentos (C#)

---
+++ Programar em C#/22-Argumentos.cs
@@ -0,0 +1,28 @@
+﻿// YouTube: https://youtu.be/mrbE0tQqbtQ
+// Publicação: https://caffeinealgorithm.com/blog/20211203/argumentos-em-csharp/
+
+using System;
+
+namespace Base {
+  class Argumentos {
+    public void Run() {
+      DadosPessoais("Nelson Silva", 25, "Portuguesa");
+      DadosPessoais("Larissa Fernandes", 37, "Brasileira");
+    }
+
+    public void DadosPessoais(string nome, int idade, string nacionalidade) {
+      Console.WriteLine($"Nome: {nome}");
+      Console.WriteLine($"Idade: {idade}");
+      Console.WriteLine($"Nacionalidade: {nacionalidade}");
+    }
+  }
+}
+
+/*
+  Nome: Nelson Silva
+  Idade: 25
+  Nacionalidade: Portuguesa
+  Nome: Larissa Fernandes
+  Idade: 37
+  Nacionalidade: Brasileira
+*/
