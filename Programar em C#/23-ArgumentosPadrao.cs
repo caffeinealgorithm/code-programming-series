3d7cecf39eecb6cbe491df94240a5a2356822a1d
Nelson Silva <nelsonsilvadev@gmail.com>
2021-12-06 18:31:37 +0000

    Add 23-ArgumentosPadrao (C#)

---
+++ Programar em C#/23-ArgumentosPadrao.cs
@@ -0,0 +1,22 @@
+﻿// YouTube: https://youtu.be/9Dv8IR1Hy5U
+// Publicação: https://caffeinealgorithm.com/blog/20211206/argumentos-padrao-em-csharp/
+
+using System;
+
+namespace Base {
+  class ArgumentosPadrao {
+    public void Run() {
+      EstadoDaPorta(false);
+    }
+
+    public void EstadoDaPorta(bool estado = true) {
+      if (estado)
+        Console.WriteLine("A porta está aberta.");
+      else {
+        Console.WriteLine("A porta está fechada.");
+      }
+    }
+  }
+}
+
+// A porta está fechada.
