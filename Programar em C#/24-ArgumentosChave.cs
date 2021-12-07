7e31f3251d2ffa9e0bc2491eb6d9117291e00485
Nelson Silva <nelsonsilvadev@gmail.com>
2021-12-07 18:19:18 +0000

    Add 24-ArgumentosChave (C#)

---
+++ Programar em C#/24-ArgumentosChave.cs
@@ -0,0 +1,24 @@
+﻿// YouTube: https://youtu.be/yXu3nkuCn1I
+// Publicação: https://caffeinealgorithm.com/blog/20211207/argumentos-chave-em-csharp/
+
+using System;
+
+namespace Base {
+  class ArgumentosChave {
+    public void Run() {
+      ImprimirABC(c: 1, a: 2, b: 3);
+    }
+
+    public void ImprimirABC(int a, int b, int c) {
+      Console.WriteLine($"Valor de a: {a}");
+      Console.WriteLine($"Valor de b: {b}");
+      Console.WriteLine($"Valor de c: {c}");
+    }
+  }
+}
+
+/*
+  Valor de a: 2
+  Valor de b: 3
+  Valor de c: 1
+*/
