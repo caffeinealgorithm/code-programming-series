1ec2896979890bdae4eb5666c14db217731e1096
Nelson Silva <nelsonsilvadev@gmail.com>
2021-12-09 18:31:25 +0000

    Add 25-RefEOut (C#)

---
+++ Programar em C#/25-RefEOut.cs
@@ -0,0 +1,33 @@
+﻿// YouTube: https://youtu.be/IOUI--CFDFs
+// Publicação: https://caffeinealgorithm.com/blog/20211209/ref-e-out-em-csharp/
+
+using System;
+
+namespace Base {
+  class RefEOut {
+    public void Run() {
+      int numero = 0;
+
+      Console.WriteLine($"Número antes da função: {numero}");
+      Out(out numero);
+      Console.WriteLine($"Número depois da função: {numero}");
+    }
+
+    public void Normal(int numero) {
+      numero = 1;
+    }
+
+    public void Ref(ref int numero) {
+      numero = 2;
+    }
+
+    public void Out(out int numero) {
+      numero = 3;
+    }
+  }
+}
+
+/*
+  Número antes da função: 0
+  Número depois da função: 3
+*/
