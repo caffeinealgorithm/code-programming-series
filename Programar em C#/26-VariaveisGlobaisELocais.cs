c5baa3c1c13c3ab5b8fc9551aefe9142fa8ac895
Nelson Silva <nelsonsilvadev@gmail.com>
2021-12-10 18:30:01 +0000

    Add 26-VariaveisGlobaisELocais (C#)

---
+++ Programar em C#/26-VariaveisGlobaisELocais.cs
@@ -0,0 +1,25 @@
+﻿// YouTube: https://youtu.be/nRnTx4JWWm4
+// Publicação: https://caffeinealgorithm.com/blog/20211210/variaveis-globais-e-locais-em-csharp/
+
+using System;
+
+namespace Base {
+  class VariaveisGlobaisELocais {
+    string acesso = "Global";
+
+    public void Run() {
+      MudarAcesso();
+      Console.WriteLine($"Acesso no exterior da função: {acesso}");
+    }
+
+    public void MudarAcesso() {
+      string acesso = "Local";
+      Console.WriteLine($"Acesso no interior da função: {acesso}");
+    }
+  }
+}
+
+/*
+  Acesso no interior da função: Local
+  Acesso no exterior da função: Global
+*/
