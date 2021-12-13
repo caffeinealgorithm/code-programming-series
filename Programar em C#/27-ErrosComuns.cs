20f88ee62cc774402b088604cae9fbe9cacd7ff3
Nelson Silva <nelsonsilvadev@gmail.com>
2021-12-13 01:53:00 +0000

    Add 27-ErrosComuns (C#)

---
+++ Programar em C#/27-ErrosComuns.cs
@@ -0,0 +1,25 @@
+﻿// YouTube: https://youtu.be/zTZxnegO2Ro
+// Publicação: https://caffeinealgorithm.com/blog/20211213/erros-comuns-em-csharp/
+
+using System;
+
+namespace Base {
+  class ErrosComuns {
+    public void Run() {
+      // Erro de compilação
+      Console.WriteLine("Eu sou uma string.");
+
+      // Erro de tempo de execução
+      int x = 10, y = 0;
+      // Console.WriteLine($"Resultado da divisão: {x / y}");
+
+      // Erro de lógica
+      string primeiroNome = string.Empty, ultimoNome = string.Empty;
+
+      Console.Write("Insere o teu último nome: ");
+      ultimoNome = Console.ReadLine();
+
+      Console.WriteLine($"Nome: {primeiroNome} {ultimoNome}");
+    }
+  }
+}
