0dec1e1811f7b62a283d48c899c0d35cf5d0a6c0
Nelson Silva <nelsonsilvadev@gmail.com>
2021-12-16 18:19:22 +0000

    Add 30-MaisSobreAsVariaveis (C#)

---
+++ Programar em C#/30-MaisSobreAsVariaveis.cs
@@ -0,0 +1,31 @@
+﻿// YouTube: https://youtu.be/uMbksPhe4m0
+// Publicação: https://caffeinealgorithm.com/blog/20211216/mais-sobre-as-variaveis-em-csharp/
+
+using System;
+
+namespace Base {
+  class MaisSobreAsVariaveis {
+    public void Run() {
+      /*
+        byte _byte = 255;
+        sbyte _sbyte = 127;
+        decimal _decimal = 10.0M;
+        float _float = 10.0F;
+        uint _uint = 4000000000;
+        long _long = 9000000000000000000;
+        ulong _ulong = 18000000000000000000;
+        short _short = 32000;
+        ushort _ushort = 65000;
+      */
+
+      var variavel = "Eu sou uma string.";
+      Console.WriteLine($"Conteúdo inicial: {variavel}");
+      Console.WriteLine($"Conteúdo final: {variavel}");
+    }
+  }
+}
+
+/*
+  Conteúdo inicial: Eu sou uma string.
+  Conteúdo final: Eu sou uma string.
+*/
