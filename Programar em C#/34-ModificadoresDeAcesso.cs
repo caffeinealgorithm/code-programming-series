529ea1b8e0cacce048a4b1a555262592e3d3ac25
Nelson Silva <nelsonsilvadev@gmail.com>
2021-12-22 18:26:08 +0000

    Add 34-ModificadoresDeAcesso (C#)

---
+++ Programar em C#/34-ModificadoresDeAcesso.cs
@@ -0,0 +1,13 @@
+﻿// YouTube: https://youtu.be/Uz-wyfqr9yU
+// Publicação: https://caffeinealgorithm.com/blog/20211222/modificadores-de-acesso-em-csharp/
+
+using System;
+
+namespace Base {
+  class ModificadoresDeAcesso {
+    // public, protected, internal, protected internal e private
+    private void Run() {
+      Console.WriteLine("Estamos a tentar aprender a trabalhar com os vários modificadores de acesso.");
+    }
+  }
+}
