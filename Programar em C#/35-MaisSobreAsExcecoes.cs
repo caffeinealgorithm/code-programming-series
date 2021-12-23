6381ad96491c94e5d73c62173abe5765f8278ecf
Nelson Silva <nelsonsilvadev@gmail.com>
2021-12-23 17:56:53 +0000

    Add 35-MaisSobreAsExcecoes (C#)

---
+++ Programar em C#/35-MaisSobreAsExcecoes.cs
@@ -0,0 +1,24 @@
+﻿// YouTube: https://youtu.be/06RgzwoqPDU
+// Publicação: https://caffeinealgorithm.com/blog/20211223/mais-sobre-as-excecoes-em-csharp/
+
+using System;
+
+namespace Base {
+  class MaisSobreAsExcecoes {
+    private Exception stringVazia = new Exception("Não se pode verificar uma string que esteja vazia.");
+
+    public void Run() {
+      try {
+        VerificarString("Nelson Silva");
+      }
+      catch (Exception excecao) {
+        Console.WriteLine(excecao);
+      }
+    }
+
+    private void VerificarString(string _string) {
+      if (_string == string.Empty)
+        throw stringVazia;
+    }
+  }
+}
