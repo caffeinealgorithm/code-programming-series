20d97de71a1a1798bf1e31dc427aa605e6eeb495
Nelson Silva <nelsonsilvadev@gmail.com>
2021-12-29 18:41:33 +0000

    Add 40-Struct (C#)

---
+++ Programar em C#/40-Struct.cs
@@ -0,0 +1,38 @@
+﻿// YouTube: https://youtu.be/tyvCf7CUEfg
+// Publicação: https://caffeinealgorithm.com/blog/20211230/struct-em-csharp/
+
+using System;
+
+namespace Base {
+  class Struct {
+    public void Run() {
+      var estrutura = new Estrutura();
+      estrutura.Numero = 200;
+
+      // O número precisa de ser >= 0 e <= 100.
+    }
+  }
+
+  struct Estrutura : INumero {
+    private int numero;
+
+    public int Numero {
+      set {
+        if (value >= 0 && value <= 100) {
+          numero = value;
+          ImprimirNumero();
+        }
+        else
+          Console.WriteLine("O número precisa de ser >= 0 e <= 100.");
+      }
+    }
+
+    public void ImprimirNumero() {
+      Console.WriteLine($"Número: {numero}");
+    }
+  }
+
+  interface INumero {
+    void ImprimirNumero();
+  }
+}
