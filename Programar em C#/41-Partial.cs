35630c84fc68a86bae2f621330c275dd63ca23e7
Nelson Silva <nelsonsilvadev@gmail.com>
2021-12-31 18:12:52 +0000

    Add 41-Partial (C#)

---
+++ Programar em C#/41-Partial.cs
@@ -0,0 +1,37 @@
+﻿// YouTube: https://youtu.be/xwk4Xx5-zO0
+// Publicação: https://caffeinealgorithm.com/blog/20211231/partial-em-csharp/
+
+using System;
+
+namespace Base {
+  class Partial {
+    public void Run() {
+      var pessoa = new Pessoa();
+      pessoa.Informacao();
+
+      /*
+        Nome: Nelson Silva
+        Idade: 25
+      */
+    }
+  }
+
+  partial class Pessoa {
+    private string primeiroNome = "Nelson";
+    partial void _Informacao();
+
+    public void Informacao() {
+      _Informacao();
+    }
+  }
+
+  partial class Pessoa {
+    private string ultimoNome = "Silva";
+    private int idade = 25;
+
+    partial void _Informacao() {
+      Console.WriteLine($"Nome: {primeiroNome} {ultimoNome}");
+      Console.WriteLine($"Idade: {idade}");
+    }
+  }
+}
