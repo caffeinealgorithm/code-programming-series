8f4936170b4c455bbdec281bc08636d786e76802
Nelson Silva <nelsonsilvadev@gmail.com>
2022-01-03 18:33:56 +0000

    Add 42-Abstract (C#)

---
+++ Programar em C#/42-Abstract.cs
@@ -0,0 +1,52 @@
+﻿// YouTube: https://youtu.be/kic0anFXVUs
+// Publicação: https://caffeinealgorithm.com/blog/20220103/abstract-em-csharp/
+
+using System;
+using System.Collections.Generic;
+
+namespace Base {
+  class Abstract {
+    public void Run() {
+      // ClasseX.InformacaoX();
+      var classe = new ClasseY();
+      classe.InformacaoY();
+
+      /*
+        País: Portugal
+        País: Brasil
+        País: Espanha
+        País: França
+        País: Itália
+        País: Austrália
+        País: Índia
+      */
+    }
+  }
+
+  abstract class ClasseX {
+    public static string organizacao = "Caffeine Algorithm";
+    public static List<string> paises = new List<string>() {
+      "Portugal",
+      "Brasil",
+      "Espanha",
+      "França",
+      "Itália",
+      "Austrália",
+      "Índia"
+    };
+
+    public static void InformacaoX() {
+      foreach (var carater in organizacao)
+        Console.WriteLine($"Carater: {carater}");
+    }
+
+    public abstract void InformacaoY();
+  }
+
+  class ClasseY : ClasseX {
+    public override void InformacaoY() {
+      foreach (var pais in paises)
+        Console.WriteLine($"País: {pais}");
+    }
+  }
+}
