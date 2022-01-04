5d183b390d3d0a0f0881b3079ecc5713d0f13473
Nelson Silva <nelsonsilvadev@gmail.com>
2022-01-04 18:22:44 +0000

    Add 43-Delegate (C#)

---
+++ Programar em C#/43-Delegate.cs
@@ -0,0 +1,38 @@
+﻿// YouTube: https://youtu.be/bbXsVSa952Y
+// Publicação: https://caffeinealgorithm.com/blog/20220104/delegate-em-csharp/
+
+using System;
+
+namespace Base {
+  class Delegate {
+    private delegate void ConjuntoDeMetodos(string nome);
+
+    public void Run() {
+      Console.Write("Insere o teu primeiro e último nome: ");
+      var nome = Console.ReadLine();
+
+      var conjuntoDeMetodos = new ConjuntoDeMetodos(ImprimirNome);
+      conjuntoDeMetodos += ImprimirNomeEmMaiusculas;
+      conjuntoDeMetodos += ImprimirNomeEmMinusculas;
+      conjuntoDeMetodos(nome);
+    }
+
+    public void ImprimirNome(string nome) {
+      Console.WriteLine($"Nome: {nome}");
+    }
+
+    public void ImprimirNomeEmMaiusculas(string nome) {
+      Console.WriteLine($"Nome em maiúsculas: {nome.ToUpper()}");
+    }
+
+    public void ImprimirNomeEmMinusculas(string nome) {
+      Console.WriteLine($"Nome em minúsculas: {nome.ToLower()}");
+    }
+  }
+}
+
+/*
+  Nome: Nelson Silva
+  Nome em maiúsculas: NELSON SILVA
+  Nome em minúsculas: nelson silva
+*/
