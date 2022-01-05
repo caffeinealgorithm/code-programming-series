83c5a6f3ab95073b450c8d4f391b5dcf991808b4
Nelson Silva <nelsonsilvadev@gmail.com>
2022-01-05 17:37:33 +0000

    Add 44-IsEAs (C#)

---
+++ Programar em C#/44-IsEAs.cs
@@ -0,0 +1,28 @@
+﻿// YouTube: https://youtu.be/2kgNJVpXB40
+// Publicação: https://caffeinealgorithm.com/blog/20220105/is-e-as-em-csharp/
+
+using System;
+
+namespace Base {
+  class IsEAs {
+    private string organizacao = "Caffeine Algorithm", nome = string.Empty;
+    private object _nome = "Nelson Silva";
+
+    public void Run() {
+      if (organizacao is string)
+        Console.WriteLine("O atributo \"organizacao\" é do tipo string.");
+      else
+        Console.WriteLine("O atributo \"organizacao\" não é do tipo string.");
+
+      nome = _nome as string;
+
+      if (nome is string)
+        Console.WriteLine($"Nome: {nome}");
+    }
+  }
+}
+
+/*
+  O atributo "organizacao" é do tipo string.
+  Nome: Nelson Silva
+*/
