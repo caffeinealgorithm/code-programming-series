6d4de70cc24cc22879ff5e7c94b4e75083190cef
Nelson Silva <nelsonsilvadev@gmail.com>
2022-01-06 15:01:40 +0000

    Add 45-ClasseConvert (C#)

---
+++ Programar em C#/45-ClasseConvert.cs
@@ -0,0 +1,34 @@
+﻿// YouTube: https://youtu.be/7nJbm-CN2XY
+// Publicação: https://caffeinealgorithm.com/blog/20220106/classe-convert-em-csharp/
+
+using System;
+
+namespace Base {
+  class ClasseConvert {
+    private string letra = "N", anoAtual = "2021";
+    private char _letra = ' ';
+    private int _anoAtual = 0;
+
+    public void Run() {
+      try {
+        _letra = Convert.ToChar(letra);
+        _anoAtual = Convert.ToInt32(anoAtual);
+      }
+      catch (Exception excecao) {
+        Console.WriteLine(excecao);
+      }
+      finally {
+        if (_letra != ' ')
+          Console.WriteLine($"Letra: {_letra}");
+
+        if (_anoAtual != 0)
+          Console.WriteLine($"Ano atual: {_anoAtual}");
+      }
+    }
+  }
+}
+
+/*
+  Letra: N
+  Ano atual: 2021
+*/
