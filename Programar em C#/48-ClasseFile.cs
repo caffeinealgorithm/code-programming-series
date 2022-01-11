2044e4cc048ca27f2a95ea0ba74e022d51adb55e
Nelson Silva <nelsonsilvadev@gmail.com>
2022-01-11 18:48:20 +0000

    Add 48-ClasseFile (C#)

---
+++ Programar em C#/48-ClasseFile.cs
@@ -0,0 +1,25 @@
+﻿// YouTube: https://youtu.be/jW8uXGlFwZo
+// Publicação: https://caffeinealgorithm.com/blog/20220111/classe-file-em-csharp/
+
+using System;
+using System.IO;
+
+namespace Base {
+  class ClasseFile {
+    public void Run() {
+      // File.Create("Ficheiro-Create.txt");
+      // File.Copy("Ficheiro-Create.txt", "Ficheiro-Copy.txt", true);
+      // File.Delete("Ficheiro-Copy.txt");
+      // File.Move("Ficheiro-Create.txt", "Ficheiro-Move.txt");
+
+      File.Delete("Ficheiro-Move.txt");
+
+      if (File.Exists("Ficheiro-Move.txt"))
+        Console.WriteLine("O ficheiro \"Ficheiro-Move.txt\" existe.");
+      else
+        Console.WriteLine("O ficheiro \"Ficheiro-Move.txt\" não existe.");
+    }
+  }
+}
+
+// O ficheiro "Ficheiro-Move.txt" não existe.
