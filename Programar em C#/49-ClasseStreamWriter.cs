5cf3f11ab16470669c634bbb143b895c8f224c31
Nelson Silva <nelsonsilvadev@gmail.com>
2022-01-12 18:25:34 +0000

    Add 49-ClasseStreamWriter (C#)

---
+++ Programar em C#/49-ClasseStreamWriter.cs
@@ -0,0 +1,36 @@
+﻿// YouTube: https://youtu.be/7Hjm23B-quw
+// Publicação: https://caffeinealgorithm.com/blog/20220112/classe-streamwriter-em-csharp/
+
+using System.Collections.Generic;
+using System.IO;
+
+namespace Base {
+  class ClasseStreamWriter {
+    private StreamWriter escreverFicheiro;
+    private List<string> paises = new List<string>() {
+      "Portugal",
+      "Brasil",
+      "Espanha",
+      "França",
+      "Itália",
+      "Austrália",
+      "Índia"
+    };
+
+    public void Run() {
+      /*
+        escreverFicheiro = new StreamWriter("Ficheiro.txt");
+
+        foreach (var pais in paises)
+          escreverFicheiro.WriteLine(pais);
+
+        escreverFicheiro.Close();
+      */
+
+      using (escreverFicheiro = new StreamWriter("Ficheiro.txt")) {
+        foreach (var pais in paises)
+          escreverFicheiro.WriteLine(pais);
+      }
+    }
+  }
+}
