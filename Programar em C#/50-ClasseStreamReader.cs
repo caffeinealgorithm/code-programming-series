10023015b94c53769bd680ea3bd2a5a4311bd015
Nelson Silva <nelsonsilvadev@gmail.com>
2022-01-13 16:01:58 +0000

    Add 50-ClasseStreamReader (C#)

---
+++ Programar em C#/50-ClasseStreamReader.cs
@@ -0,0 +1,34 @@
+﻿// YouTube: https://youtu.be/NpjrizaL77w
+// Publicação: https://caffeinealgorithm.com/blog/20220113/classe-streamreader-em-csharp/
+
+using System;
+using System.IO;
+
+namespace Base {
+  class ClasseStreamReader {
+    private StreamReader lerFicheiro;
+
+    public void Run() {
+      using (lerFicheiro = new StreamReader("Ficheiro.txt")) {
+        var linha = string.Empty;
+
+        /*
+          while ((linha = lerFicheiro.ReadLine()) != null)
+            Console.WriteLine(linha);
+        */
+
+        Console.WriteLine(lerFicheiro.ReadToEnd());
+      }
+    }
+  }
+}
+
+/*
+  Portugal
+  Brasil
+  Espanha
+  França
+  Itália
+  Austrália
+  Índia
+*/
