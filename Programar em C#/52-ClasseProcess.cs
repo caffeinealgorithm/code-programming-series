8b49db20b81d3a328925f347f3a26f3fe90c379a
Nelson Silva <nelsonsilvadev@gmail.com>
2022-01-17 17:18:24 +0000

    Add 52-ClasseProcess (C#)

---
+++ Programar em C#/52-ClasseProcess.cs
@@ -0,0 +1,43 @@
+﻿// YouTube: https://youtu.be/yXHX-nA4Gsk
+// Publicação: https://caffeinealgorithm.com/blog/20220117/classe-process-em-csharp/
+
+using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Base {
+  class ClasseProcess {
+    // private Process processo;
+    private Process[] processos;
+
+    public void Run() {
+      /*
+        processo = Process.Start("notepad.exe");
+        processo.Kill();
+
+        Console.Write("Pesquisar por: ");
+        var pesquisa = Console.ReadLine();
+        PesquisarNoGoogle(pesquisa);
+      */
+
+      processos = Process.GetProcessesByName("chrome");
+      Console.WriteLine($"Número de processos: {processos.Count()}");
+
+      foreach (var processo in processos)
+        Console.WriteLine($"Nome do processo: {processo.ProcessName}");
+    }
+
+    private void PesquisarNoGoogle(string pesquisa) {
+      var aplicacao = @"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe";
+      Process.Start(aplicacao, $"https://www.google.com/search?q={pesquisa}".Replace(' ', '+'));
+    }
+  }
+}
+
+/*
+  Número de processos: 28
+  Nome do processo: chrome
+  Nome do processo: chrome
+  Nome do processo: chrome
+  ...
+*/
