d8e9d95bf86ae4f1a9e31b63b54b710c1e8431fc
Nelson Silva <nelsonsilvadev@gmail.com>
2022-01-18 18:24:33 +0000

    Add 53-ClasseThread (C#)

---
+++ Programar em C#/53-ClasseThread.cs
@@ -0,0 +1,54 @@
+﻿// YouTube: https://youtu.be/N1ZSoxB80B0
+// Publicação: https://caffeinealgorithm.com/blog/20220118/classe-thread-em-csharp/
+
+using System;
+using System.Threading;
+
+namespace Base {
+  class ClasseThread {
+    private Thread tarefa;
+
+    public void Run() {
+      tarefa = new Thread(ExecutarTarefa);
+      tarefa.Start();
+
+      for (int indice = 1; indice <= 5; indice++) {
+        Console.WriteLine($"Run() #{indice}");
+        Thread.Sleep(1000);
+      }
+
+      Console.WriteLine("A tarefa Run() finalizou.");
+
+      tarefa.Join();
+
+      Console.WriteLine("A tarefa ExecutarTarefa() finalizou.");
+    }
+
+    private void ExecutarTarefa() {
+      for (int indice = 1; indice <= 10; indice++) {
+        Console.WriteLine($"ExecutarTarefa() #{indice}");
+        Thread.Sleep(1000);
+      }
+    }
+  }
+}
+
+/*
+  Run() #1
+  ExecutarTarefa() #1
+  Run() #2
+  ExecutarTarefa() #2
+  Run() #3
+  ExecutarTarefa() #3
+  Run() #4
+  ExecutarTarefa() #4
+  Run() #5
+  ExecutarTarefa() #5
+  A tarefa Run() finalizou.
+  ExecutarTarefa() #6
+  ExecutarTarefa() #7
+  ExecutarTarefa() #8
+  ExecutarTarefa() #9
+  ExecutarTarefa() #10
+  A tarefa ExecutarTarefa() finalizou.
+*/
