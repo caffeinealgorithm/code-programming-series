bac8ac22035b7cda00f564891117ac647676ecf9
Nelson Silva <nelsonsilvadev@gmail.com>
2022-01-19 15:45:48 +0000

    Add 54-ClasseDebug (C#)

---
+++ Programar em C#/54-ClasseDebug.cs
@@ -0,0 +1,21 @@
+﻿// YouTube: https://youtu.be/DU1TJ_QGS_s
+// Publicação: https://caffeinealgorithm.com/blog/20220119/classe-debug-em-csharp/
+
+using System.Diagnostics;
+
+namespace Base {
+  class ClasseDebug {
+    private string[] cores = new string[] {
+      "Azul",
+      "Verde",
+      "Amarelo",
+      "Vermelho",
+      "Laranja"
+    };
+
+    public void Run() {
+      for (int indice = 0; indice < cores.Length; indice++)
+        Debug.WriteLine($"cores[{indice}]: {cores[indice]}");
+    }
+  }
+}
