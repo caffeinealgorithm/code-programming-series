d7c2af5acfece9b8a9f3645f37a409d317554e64
Nelson Silva <nelsonsilvadev@gmail.com>
2022-01-21 17:55:16 +0000

    Add 56-ClasseHashtable (C#)

---
+++ Programar em C#/56-ClasseHashtable.cs
@@ -0,0 +1,40 @@
+﻿// YouTube: https://youtu.be/QnhCVLqYcJM
+// Publicação: https://caffeinealgorithm.com/blog/20220121/classe-hashtable-em-csharp/
+
+using System;
+using System.Collections;
+
+namespace Base {
+  class ClasseHashtable {
+    private Hashtable pessoas = new Hashtable() {
+      { "Nelson Silva", 25 },
+      { "Larissa Fernandes", 37 }
+    };
+
+    public void Run() {
+      pessoas.Add("Pedro Henrique", 52);
+      pessoas.Add("Raquel Soares", 68);
+
+      pessoas["Pedro Henrique"] = 100;
+      pessoas.Remove("Larissa Fernandes");
+      pessoas.Clear();
+
+      Console.WriteLine($"Número de pessoas: {pessoas.Count}");
+
+      foreach (DictionaryEntry pessoa in pessoas) {
+        Console.WriteLine($"Nome: {pessoa.Key}");
+        Console.WriteLine($"Idade: {pessoa.Value}");
+      }
+    }
+  }
+}
+
+/*
+  Número de pessoas: 3
+  Nome: Raquel Soares
+  Idade: 68
+  Nome: Nelson Silva
+  Idade: 25
+  Nome: Pedro Henrique
+  Idade: 100
+*/
