5997e39087ffbdb1686fe3bd84fb85fded1da068
Nelson Silva <nelsonsilvadev@gmail.com>
2022-01-27 23:05:49 +0000

    Add 61-MetodosIII (C#)

---
+++ Programar em C#/61-MetodosIII.cs
@@ -0,0 +1,29 @@
+﻿// YouTube: https://youtu.be/x1sAO_ePZdI
+// Publicação: https://caffeinealgorithm.com/blog/20220128/metodos-iii-em-csharp/
+
+using System;
+
+namespace Base {
+  class MetodosIII {
+    private string abecedario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ", nome = "Nelson | Silva";
+
+    public void Run() {
+      // Remove()
+      Console.WriteLine($"Abecedário até à letra N: {abecedario.Remove(14)}");
+
+      var indiceX = nome.IndexOf(' ');
+      var indiceY = nome.IndexOf(' ', indiceX + 1);
+      Console.WriteLine($"Nome: {nome.Remove(indiceX, indiceY - indiceX)}");
+
+      // Replace()
+      var lingProgPreferida = "A minha linguagem de programação preferida já foi o C#.";
+      Console.WriteLine(lingProgPreferida.Replace("já foi", "é"));
+    }
+  }
+}
+
+/*
+  Abecedário até à letra N: ABCDEFGHIJKLMN
+  Nome: Nelson Silva
+  A minha linguagem de programaçao preferida é o C#.
+*/
