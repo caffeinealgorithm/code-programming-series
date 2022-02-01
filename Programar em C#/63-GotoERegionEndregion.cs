e86e140ea860fe8ca49089a259ae3d8ff26240b5
Nelson Silva <nelsonsilvadev@gmail.com>
2022-02-01 18:09:15 +0000

    Add 63-GotoERegionEndregion (C#)

---
+++ Programar em C#/63-GotoERegionEndregion.cs
@@ -0,0 +1,40 @@
+﻿// YouTube: https://youtu.be/lbgVj7NBFAg
+// Publicação: https://caffeinealgorithm.com/blog/20220201/goto-e-region-endregion-em-csharp/
+
+#region Bibliotecas
+using System;
+#endregion
+
+namespace Base {
+  class GotoERegionEndregion {
+    #region Método Run()
+    public void Run() {
+      Console.WriteLine($"Resultado do método TestarGoto(): {TestarGoto()}");
+    }
+    #endregion
+
+    #region Método TestarGoto()
+    private int TestarGoto() {
+      var numero = 0;
+
+      for (int i = 1; i <= 10; i++) {
+        for (int j = i; j <= 10; j++) {
+          for (int k = 0; k <= 10; k++) {
+            if (j == 5 && k == 5)
+              goto PrimeiroCiclo;
+          }
+
+          numero++;
+        }
+
+        PrimeiroCiclo:
+          continue;
+      }
+
+      return numero;
+    }
+    #endregion
+  }
+}
+
+// Resultado do método TestarGoto(): 25
