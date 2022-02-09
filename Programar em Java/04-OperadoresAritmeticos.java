03079ba3f86fad908d101cdfbf92bea996d7ce98
Nelson Silva <nelsonsilvadev@gmail.com>
2022-02-09 17:49:36 +0000

    Add 04-OperadoresAritmeticos (Java)

---
+++ Programar em Java/04-OperadoresAritmeticos.java
@@ -0,0 +1,14 @@
+// YouTube: https://youtu.be/USlZetbXrA
+// Publicação: https://caffeinealgorithm.com/blog/20220209/operadores-aritmeticos-em-java/
+
+package com.caffeinealgorithm.programaremjava;
+
+public class OperadoresAritmeticos {
+  public void Run() {
+    System.out.println("Adição: " + (10 + 10)); // Adição: 20
+    System.out.println("Subtração: " + (10.5 - 5.5 - 4.5)); // Subtração: 0.5
+    System.out.println("Multiplicação: " + (-10 * 2)); // Multiplicação: -20
+    System.out.println("Divisão: " + (10 / 2)); // Divisão: 5
+    System.out.println("Resto: " + (8 % 2)); // Resto: 0
+  }
+}
