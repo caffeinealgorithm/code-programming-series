bfb7c04f219d8ed27491a8a3789e9819ce3a96bd
Nelson Silva <nelsonsilvadev@gmail.com>
2022-02-10 18:27:30 +0000

    Add 05-Variaveis (Java)

---
+++ Programar em Java/05-Variaveis.java
@@ -0,0 +1,20 @@
+// YouTube: https://youtu.be/Pw2Tsh8CAaE
+// Publicação: https://caffeinealgorithm.com/blog/20220210/variaveis-em-java/
+
+package com.caffeinealgorithm.programaremjava;
+
+public class Variaveis {
+  public void Run() {
+    int inteiro = 10;
+    double _double = 10.5;
+    String string = "Eu sou uma string.";
+    char carater = 'c';
+    boolean booleano = false;
+
+    System.out.println("inteiro: " + inteiro); // inteiro: 10
+    System.out.println("double: " + _double); // double: 10.5
+    System.out.println("string: " + string); // string: Eu sou uma string.
+    System.out.println("carater: " + carater); // carater: c
+    System.out.println("booleano: " + booleano); // booleano: false
+  }
+}
