37256b92970a47aaa45ce92b47f326b6719deee5
Nelson Silva <nelsonsilvadev@gmail.com>
2022-02-15 18:28:24 +0000

    Add 08-Comentarios (Java)

---
+++ Programar em Java/08-Comentarios.java
@@ -0,0 +1,20 @@
+// YouTube: https://youtu.be/skX2jrCiqdI
+// Publicação: https://caffeinealgorithm.com/blog/20220215/comentarios-em-java/
+
+package com.caffeinealgorithm.programaremjava;
+
+public class Comentarios {
+  public void Run() {
+    // Eu sou um comentário.
+
+    System.out.println("Eu sou uma string.");
+
+    /*
+      Eu também sou um comentário.
+      Eu faço parte deste mesmo comentário.
+      Continuo a fazer parte dele.
+    */
+
+    System.out.println("Eu continuo a ser uma string.");
+  }
+}
