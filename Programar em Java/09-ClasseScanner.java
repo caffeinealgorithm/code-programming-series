75093fa28cf79649ab569151d9639a0494dfa061
Nelson Silva <nelsonsilvadev@gmail.com>
2022-02-16 18:29:16 +0000

    Add 09-ClasseScanner (Java)

---
+++ Programar em Java/09-ClasseScanner.java
@@ -0,0 +1,25 @@
+// YouTube: https://youtu.be/d1Fp1_jjPsE
+// Publicação: https://caffeinealgorithm.com/blog/20220216/classe-scanner-em-java/
+
+package com.caffeinealgorithm.programaremjava;
+
+import java.util.Scanner;
+
+public class ClasseScanner {
+  public void Run() {
+    Scanner inputDoUtilizador = new Scanner(System.in);
+    String primeiroNome, ultimoNome;
+    int idade;
+
+    System.out.print("Insere o teu primeiro nome: ");
+    primeiroNome = inputDoUtilizador.nextLine();
+
+    System.out.print("Insere o teu último nome: ");
+    ultimoNome = inputDoUtilizador.nextLine();
+
+    System.out.print("Insere a tua idade: ");
+    idade = inputDoUtilizador.nextInt();
+
+    System.out.printf("Nome: %s %s\nIdade: %d", primeiroNome, ultimoNome, idade);
+  }
+}
