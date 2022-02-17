d7f9b559498485503296c7d632d53e11e9edab47
Nelson Silva <nelsonsilvadev@gmail.com>
2022-02-17 17:34:50 +0000

    Add 10-Arrays (Java)

---
+++ Programar em Java/10-Arrays.java
@@ -0,0 +1,30 @@
+// YouTube: https://youtu.be/aD7U69fw9hQ
+// Publicação: https://caffeinealgorithm.com/blog/20220217/arrays-em-java/
+
+package com.caffeinealgorithm.programaremjava;
+
+public class Arrays {
+  public void Run() {
+    /*
+      String[] cores = new String[5];
+
+      cores[0] = "Azul";
+      cores[1] = "Verde";
+      cores[2] = "Amarelo";
+      cores[3] = "Vermelho";
+      cores[4] = "Laranja";
+    */
+
+    String[] cores = new String[] {
+      "Azul",
+      "Verde",
+      "Amarelo",
+      "Vermelho",
+      "Laranja"
+    };
+
+    System.out.printf("Número de cores: %d\n", cores.length); // Número de cores: 5
+    System.out.printf("Primeira cor: %s\n", cores[0]); // Primeira cor: Azul
+    System.out.printf("Última cor: %s", cores[cores.length - 1]); // Última cor: Laranja
+  }
+}
