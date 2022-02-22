184e345d5a48f4ff54e441ef5b03a7c91ac54a75
Nelson Silva <nelsonsilvadev@gmail.com>
2022-02-22 18:11:31 +0000

    Add 13-OperadoresDeComparacao (Java)

---
+++ Programar em Java/13-OperadoresDeComparacao.java
@@ -0,0 +1,18 @@
+// YouTube: https://youtu.be/Bxg4aC54Jgs
+// Publicação: https://caffeinealgorithm.com/blog/20220222/operadores-de-comparacao-em-java/
+
+package com.caffeinealgorithm.programaremjava;
+
+public class OperadoresDeComparacao {
+  public void Run() {
+    int x = 10;
+    int y = 20;
+
+    if (x <= y || x == y)
+      System.out.println("Esta condição é verdadeira.");
+    else
+      System.out.println("Esta condição é falsa.");
+  }
+}
+
+// Esta condição é verdadeira.
