6f1442655ea7d56e2bc1044a7d509137982c31ad
Nelson Silva <nelsonsilvadev@gmail.com>
2022-03-02 17:46:58 +0000

    Add 19-BreakEContinue (Java)

---
+++ Programar em Java/19-BreakEContinue.java
@@ -0,0 +1,51 @@
+// YouTube: https://youtu.be/Wbe6-a8go0o
+// Publicação: https://caffeinealgorithm.com/blog/20220302/break-e-continue-em-java/
+
+package com.caffeinealgorithm.programaremjava;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BreakEContinue {
+  public void Run() {
+    List<String> animais = new ArrayList<>();
+    int contador = 0;
+
+    animais.add("Cão");
+    animais.add("Gato");
+    animais.add("Galinha");
+    animais.add("Coelho");
+    animais.add("Leão");
+
+    for (String animal : animais) {
+      System.out.printf("Animal: %s\n", animal);
+
+      if (animal == "Galinha")
+        break;
+    }
+
+    while (contador < 10) {
+      contador++;
+
+      if (contador == 5)
+        continue;
+
+      System.out.printf("Contador: %d\n", contador);
+    }
+  }
+}
+
+/*
+  Animal: Cão
+  Animal: Gato
+  Animal: Galinha
+  Contador: 1
+  Contador: 2
+  Contador: 3
+  Contador: 4
+  Contador: 6
+  Contador: 7
+  Contador: 8
+  Contador: 9
+  Contador: 10
+*/
