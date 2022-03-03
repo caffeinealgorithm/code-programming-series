3c546d78fa06ee65ca7f34bc5542c95f844fc8cd
Nelson Silva <nelsonsilvadev@gmail.com>
2022-03-03 18:28:28 +0000

    Add 20-Funcoes (Java)

---
+++ Programar em Java/20-Funcoes.java
@@ -0,0 +1,30 @@
+// YouTube: https://youtu.be/BI1mwKihlJI
+// Publicação: https://caffeinealgorithm.com/blog/20220303/funcoes-em-java/
+
+package com.caffeinealgorithm.programaremjava;
+
+public class Funcoes {
+  public void Run() {
+    dadosPessoais();
+    dadosPessoais();
+    dadosPessoais();
+  }
+
+  public void dadosPessoais() {
+    System.out.println("Nome: Nelson Silva");
+    System.out.println("Idade: 25");
+    System.out.println("Nacionalidade: Portuguesa");
+  }
+}
+
+/*
+  Nome: Nelson Silva
+  Idade: 25
+  Nacionalidade: Portuguesa
+  Nome: Nelson Silva
+  Idade: 25
+  Nacionalidade: Portuguesa
+  Nome: Nelson Silva
+  Idade: 25
+  Nacionalidade: Portuguesa
+*/
