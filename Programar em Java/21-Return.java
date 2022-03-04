7c553e981fc71680b762e97e6f3d42b91ab27c36
Nelson Silva <nelsonsilvadev@gmail.com>
2022-03-04 18:08:33 +0000

    Add 22-Argumentos (Java)

---
+++ Programar em Java/21-Return.java
@@ -0,0 +1,21 @@
+// YouTube: https://youtu.be/hnoGygkv9fw
+// Publicação: https://caffeinealgorithm.com/blog/20220304/return-em-java/
+
+package com.caffeinealgorithm.programaremjava;
+
+public class Return {
+  public void Run() {
+    System.out.printf("Resultado da adição: %d", adicao());
+  }
+
+  public int adicao() {
+    int resultado = 0;
+
+    for (int valor = 1; valor <= 10; valor++)
+      resultado += valor;
+
+    return resultado;
+  }
+}
+
+// Resultado da adição: 55
