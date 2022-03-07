797ed7c95812a0579195f94ed69ea4b2a66446f2
Nelson Silva <nelsonsilvadev@gmail.com>
2022-03-07 18:34:54 +0000

    Add 22-Argumentos (Java)

---
+++ Programar em Java/22-Argumentos.java
@@ -0,0 +1,26 @@
+// YouTube: https://youtu.be/k76K3UraNhU
+// Publicação: https://caffeinealgorithm.com/blog/20220307/argumentos-em-java/
+
+package com.caffeinealgorithm.programaremjava;
+
+public class Argumentos {
+  public void Run() {
+    dadosPessoais("Nelson Silva", 25, "Portuguesa");
+    dadosPessoais("Larissa Fernandes", 37, "Brasileira");
+  }
+
+  public void dadosPessoais(String nome, int idade, String nacionalidade) {
+    System.out.printf("Nome: %s\n", nome);
+    System.out.printf("Idade: %d\n", idade);
+    System.out.printf("Nacionalidade: %s", nacionalidade);
+  }
+}
+
+/*
+  Nome: Nelson Silva
+  Idade: 25
+  Nacionalidade: Portuguesa
+  Nome: Larissa Fernandes
+  Idade: 37
+  Nacionalidade: Brasileira
+*/
