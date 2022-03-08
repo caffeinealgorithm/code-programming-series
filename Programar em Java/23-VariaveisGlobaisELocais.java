41a4f7094bce63720d083e4d89da18272b04b084
Nelson Silva <nelsonsilvadev@gmail.com>
2022-03-08 18:03:06 +0000

    Add 23-VariaveisGlobaisELocais (Java)

---
+++ Programar em Java/23-VariaveisGlobaisELocais.java
@@ -0,0 +1,23 @@
+// YouTube: https://youtu.be/hgVFRjqnSPM
+// Publicação: https://caffeinealgorithm.com/blog/20220308/variaveis-globais-e-locais-em-java/
+
+package com.caffeinealgorithm.programaremjava;
+
+public class VariaveisGlobaisELocais {
+  String acesso = "Global";
+
+  public void Run() {
+    mudarAcesso();
+    System.out.printf("Acesso no exterior da função: %s", acesso);
+  }
+
+  public void mudarAcesso() {
+    String acesso = "Local";
+    System.out.printf("Acesso no interior da função: %s\n", acesso);
+  }
+}
+
+/*
+  Acesso no interior da função: Local
+  Acesso no exterior da função: Global
+*/
