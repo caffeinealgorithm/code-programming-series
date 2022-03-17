9819afd73ceb2ebdeef89e665a37bd7bf3ace79e
Nelson Silva <nelsonsilvadev@gmail.com>
2022-03-17 18:35:35 +0000

    Add 30-ModificadoresDeAcesso (Java)

---
+++ Programar em Java/30-ModificadoresDeAcesso.java
@@ -0,0 +1,11 @@
+// YouTube: https://youtu.be/e19jPpY8JfA
+// Publicação: https://caffeinealgorithm.com/blog/20220317/modificadores-de-acesso-em-java/
+
+package com.caffeinealgorithm.programaremjava;
+
+public class ModificadoresDeAcesso {
+  // public, protected e private
+  private void Run() {
+    System.out.println("Estamos a tentar aprender a trabalhar com os vários modificadores de acesso.");
+  }
+}
