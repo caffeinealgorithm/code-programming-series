a09f8303fe0e739fe7e62c2e929e292267c2de85
Nelson Silva <nelsonsilvadev@gmail.com>
2022-03-22 18:46:58 +0000

    Add 33-Heranca (Java)

---
+++ Programar em Java/33-Heranca.java
@@ -0,0 +1,36 @@
+// YouTube: https://youtu.be/wFJN_36mi9M
+// Publicação: https://caffeinealgorithm.com/blog/20220322/heranca-em-java/
+
+package com.caffeinealgorithm.programaremjava;
+
+public class Heranca {
+  public void Run() {
+    var pessoa = new Filho();
+    pessoa.informacao();
+    pessoa.comidaPreferida();
+
+    /*
+      Nome: Nelson Silva
+      Idade: 25
+      A minha comida preferida é arroz de marisco.
+    */
+  }
+}
+
+class Pai {
+  protected String ultimoNome = "Silva";
+
+  public void comidaPreferida() {
+    System.out.println("A minha comida preferida é arroz de marisco.");
+  }
+}
+
+class Filho extends Pai {
+  private String primeiroNome = "Nelson";
+  private int idade = 25;
+
+  public void informacao() {
+    System.out.printf("Nome: %s %s\n", primeiroNome, ultimoNome);
+    System.out.printf("Idade: %d\n", idade);
+  }
+}
