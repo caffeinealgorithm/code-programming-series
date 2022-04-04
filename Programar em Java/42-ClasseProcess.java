de75c0ba282947b03b9eeafe26cb02f0dd3f2421
Nelson Silva <nelsonsilvadev@gmail.com>
2022-04-05 00:00:22 +0100

    Add 42-ClasseProcess (Java)

---
+++ Programar em Java/42-ClasseProcess.java
@@ -0,0 +1,30 @@
+// YouTube: https://youtu.be/rd4_ShKKqAo
+// Publicação: https://caffeinealgorithm.com/blog/20220404/classe-process-em-java/
+
+package com.caffeinealgorithm.programaremjava;
+
+public class ClasseProcess {
+  private Process processo;
+
+  public void Run() {
+    try {
+      processo = new ProcessBuilder("notepad.exe").start();
+    }
+    catch (Exception excecao) {
+      System.out.println(excecao);
+    }
+
+    try {
+      if (processo.isAlive()) {
+        System.out.println("O processo \"notepad.exe\" está a ser executado e tal execução irá terminar em cerca de três segundos.");
+        Thread.sleep(3000);
+        processo.destroy();
+      }
+    }
+    catch (InterruptedException excecao) {
+      System.out.println(excecao);
+    }
+  }
+}
+
+// O processo "notepad.exe" está a ser executado e tal execução irá terminar em cerca de três segundos.
