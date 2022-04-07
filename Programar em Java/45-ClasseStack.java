4eb7ecd58fe76259f656ac76cec503a8d26b83af
Nelson Silva <nelsonsilvadev@gmail.com>
2022-04-07 20:10:07 +0100

    Add 45-ClasseStack (Java)

---
+++ Programar em Java/45-ClasseStack.java
@@ -0,0 +1,48 @@
+// YouTube: https://youtu.be/kfCzIPNX2CA
+// Publicação: https://caffeinealgorithm.com/blog/20220407/classe-stack-em-java/
+
+package com.caffeinealgorithm.programaremjava;
+
+import java.util.Stack;
+
+public class ClasseStack {
+  private Stack<Integer> pilha = new Stack<>();
+  private int multiplicador = 10, numero = 1;
+
+  public void Run() {
+    for (int indice = 1; indice <= 5; indice++) {
+      pilha.push(numero);
+      numero *= multiplicador;
+    }
+
+    imprimirPilha();
+
+    System.out.printf("\nA remover o número %d da pilha com o método pop().\n\n", pilha.pop());
+
+    imprimirPilha();
+
+    System.out.printf("\nO número que se encontra a seguir para ser removido da pilha é o %d.", pilha.peek());
+  }
+
+  private void imprimirPilha() {
+    for (int numero : pilha)
+      System.out.println(numero);
+  }
+}
+
+/*
+  1
+  10
+  100
+  1000
+  10000
+
+  A remover o número 10000 da pilha com o método pop().
+
+  1
+  10
+  100
+  1000
+
+  O número que se encontra a seguir para ser removido da pilha é o 1000.
+*/
