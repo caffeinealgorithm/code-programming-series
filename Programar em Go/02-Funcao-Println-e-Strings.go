e481dc05a20bf9a54fe40cab85d052c365f299a3
Nelson Silva <nelsonsilvadev@gmail.com>
2022-04-19 18:11:42 +0100

    Add 02-Funcao-Println-e-Strings (Go)

---
+++ Programar em Go/02-Funcao-Println-e-Strings.go
@@ -0,0 +1,12 @@
+// YouTube: https://youtu.be/cCq9DRRNX5A
+// Publicação: https://caffeinealgorithm.com/blog/20220419/funcao-print-e-strings-em-go/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Estamos a usar a função Println() e eu sou uma string.")
+	fmt.Println("A Maria disse que o Miguel estava \"doente\".") // A Maria disse que o Miguel estava "doente".
+	fmt.Println("Caffeine", "Algorithm") // Caffeine Algorithm
+}
