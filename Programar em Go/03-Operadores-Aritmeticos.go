7619d387195420b8dd93bdf4d3e111cdf9d5a558
Nelson Silva <nelsonsilvadev@gmail.com>
2022-04-20 19:14:32 +0100

    Add 03-Operadores-Aritmeticos (Go)

---
+++ Programar em Go/03-Operadores-Aritmeticos.go
@@ -0,0 +1,14 @@
+// YouTube: https://youtu.be/PUpojD6_Swo
+// Publicação: https://caffeinealgorithm.com/blog/20220420/operadores-aritmeticos-em-go/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Adição:", (10 + 10)) // Adição: 20
+	fmt.Println("Subtração:", (10.5 - 5.5 - 4.5)) // Subtração: 0.5
+	fmt.Println("Multiplicação:", (-10 * 2)) // Multiplicação: -20
+	fmt.Println("Divisão:", (10 / 10)) // Divisão: 1.0
+	fmt.Println("Resto:", (8 % 2)) // Resto: 0
+}
