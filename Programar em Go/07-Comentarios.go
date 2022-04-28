199399e08dd73d2bc2c50d2746f3c73f89f9948e
Nelson Silva <nelsonsilvadev@gmail.com>
2022-04-28 02:26:24 +0100

    Add 07-Comentarios (Go)

---
+++ Programar em Go/07-Comentarios.go
@@ -0,0 +1,20 @@
+// YouTube: https://youtu.be/LUq1G_MLY84
+// Publicação: https://caffeinealgorithm.com/blog/20220427/comentarios-em-go/
+
+package main
+
+import "fmt"
+
+func main() {
+	// Eu sou um comentário.
+
+	fmt.Println("Eu sou uma string.")
+
+	/*
+		Eu também sou um comentário.
+		Eu faço parte deste mesmo comentário.
+		Continuo a fazer parte dele.
+	*/
+
+	fmt.Println("Eu continuo a ser uma string.")
+}
