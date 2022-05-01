7b72997a52ef8c0fa8802cde7bf635fb846e2484
Nelson Silva <nelsonsilvadev@gmail.com>
2022-05-01 18:31:16 +0100

    Add 09-Arrays (Go)

---
+++ Programar em Go/09-Arrays.go
@@ -0,0 +1,22 @@
+// YouTube: https://youtu.be/PrK3pN6geso
+// Publicação: https://caffeinealgorithm.com/blog/20220429/arrays-em-go/
+
+package main
+
+import "fmt"
+
+func main() {
+	var cores [5]string
+
+	cores[0] = "Azul"
+	cores[1] = "Verde"
+	cores[2] = "Amarelo"
+	cores[3] = "Vermelho"
+	cores[4] = "Laranja"
+
+	cores = [...]string { "Branco", "Verde", "Amarelo", "Vermelho", "Laranja" }
+
+	fmt.Printf("Número de cores: %d\n", len(cores)) // Número de cores: 5
+	fmt.Printf("Primeira cor: %s\n", cores[0]) // Primeira cor: Azul
+	fmt.Printf("Última cor: %s", cores[len(cores) - 1]) // Última cor: Laranja
+}
